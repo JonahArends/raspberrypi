f72af3daa77d48b4b1dd09b995e7f669810bca0f
Jonah <jonah.arends@traperto.com>
2024-01-17 23:29:18 +0100

    using flask

--- web/main.py
+++ web/main.py
@@ -1,1 +1,29 @@
 ##### MAIN FLASK CODE #####
+
+### IMPORTS
+import threading
+from time import sleep
+import requests
+from flask import Flask, render_template
+from turbo_flask import Turbo
+
+### VARs
+
+### APP
+app = Flask(__name__)
+turbo = Turbo(app)
+
+### UPDATE TEMPERATURE
+def update_temperature():
+    while True:
+        response = requests.get('http://sensors-backend/temperature')
+        temperature = response.text
+        turbo.push(turbo.replace('temperature', temperature))
+        sleep(1)
+
+### ROUTE
+@app.route('/')
+def root():
+    response = requests.get('http://sensors-backend/temperature')
+    temperature = response.text
+    return render_template('template/index.html', temperature=temperature)
