abbf5cba1d7a5e2fdd449596485b88eb1677d435
Jonah <122887053+JonahArends@users.noreply.github.com>
2024-01-28 21:17:10 +0000

    dev: runner script

--- run.py
+++ run.py
@@ -4,6 +4,7 @@
 import subprocess
 from threading import Thread
 from time import sleep
+import RPi.GPIO as GPIO
 
 ### START TASKS
 def run_api():
@@ -13,9 +14,16 @@ def run_web():
     subprocess.call('gunicorn -w 4 -b 0.0.0.0:8000 web.main:app', shell=True)
 
 ### THREADS
-api_thread = Thread(target=run_api)
-web_thread = Thread(target=run_web)
+def main():
+    api_thread = Thread(target=run_api)
+    web_thread = Thread(target=run_web)
 
-api_thread.start()
-sleep(1)
-web_thread.start()
+    api_thread.start()
+    sleep(1)
+    web_thread.start()
+
+### EXECUTE
+try:
+    main()
+except KeyboardInterrupt:
+    GPIO.cleanup()
