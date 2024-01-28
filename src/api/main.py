fa0fc73d000b4aebb6d90eeb19748bf83343e5c2
Jonah <122887053+JonahArends@users.noreply.github.com>
2024-01-28 20:44:47 +0000

    debug

--- src/api/main.py
+++ src/api/main.py
@@ -33,7 +33,7 @@ app = FastAPI()
 async def run_endpoint(websocket: WebSocket):
     await websocket.accept()
     if not process['id']:
-        sp = subprocess.Popen(['python3 ../raspiProject.py --run &'], stdout=subprocess.PIPE)
+        sp = subprocess.Popen(['ls -la && python3 ../raspiProject.py --run &'], stdout=subprocess.PIPE)
         process['id'] = sp.pid
         while True:
             output = sp.stdout.readline().decode('utf-8')
