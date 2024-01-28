0247ad3acf25f2e040969330c038cf7e168762d0
Jonah <122887053+JonahArends@users.noreply.github.com>
2024-01-28 20:29:01 +0000

    fix: path

--- src/api/main.py
+++ src/api/main.py
@@ -33,7 +33,7 @@ app = FastAPI()
 async def run_endpoint(websocket: WebSocket):
     await websocket.accept()
     if not process['id']:
-        sp = subprocess.Popen(['python3 ../raspiProject.py --run &'], stdout=subprocess.PIPE)
+        sp = subprocess.Popen(['python3 raspiProject.py --run &'], stdout=subprocess.PIPE)
         process['id'] = sp.pid
         while True:
             output = sp.stdout.readline().decode('utf-8')
@@ -59,7 +59,7 @@ async def test_endpoint(websocket: WebSocket):
     if process['id']:
         os.kill(process['id'], signal.SIGTERM)
     if not process['id']:
-        sp = subprocess.Popen(['python3 ./raspiProject.py --test'], stdout=subprocess.PIPE)
+        sp = subprocess.Popen(['python3 raspiProject.py --test'], stdout=subprocess.PIPE)
         process['id'] = sp.pid
         while True:
             output = sp.stdout.readline().decode('utf-8')
