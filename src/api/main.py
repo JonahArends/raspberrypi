0ee7d9c327b8c2fa18624d5d6c2e92b1b1ff4692
Jonah <122887053+JonahArends@users.noreply.github.com>
2024-01-29 17:28:08 +0000

    fix: script runs in background

--- src/api/main.py
+++ src/api/main.py
@@ -6,7 +6,7 @@ import sys
 import signal
 import select
 import subprocess
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import FileResponse
 # from fastapi.middleware.cors import CORSMiddleware
 
@@ -30,57 +30,74 @@ app = FastAPI()
 ### SECURITY
 '''to do'''
 
+### RUN PROGRAMM
+def run_script():
+    sp = subprocess.Popen(['python3 src/raspiProject.py --run &'], shell=True, stdout=subprocess.PIPE)
+    process['id'] = sp.pid
+    sp.communicate()
+
+### KILL PROGRAMM
+def kill_script():
+    sp = subprocess.Popen(['python3 src/raspiProject.py --kill'], shell=True, stdout=subprocess.PIPE)
+    sp.communicate()
+
+### TEST PROGRAMM
+def test_script():
+    sp = subprocess.Popen(['python3 src/raspiProject.py --test'], shell=True, stdout=subprocess.PIPE)
+    sp.communicate()
+
 ### START PROGRAMM
 @app.post('/run')
-async def run():
-    sp = subprocess.call(['python3 src/raspiProject.py --run'], shell=True)
-    return True
+def start_script(background_tasks: BackgroundTasks):
+    background_tasks.add_task(run_script)
+    return {"status": "Script started"}
 
 ### STOP PROGRAMM
 @app.post('/kill')
-async def kill():
-    sp = subprocess.call(['python3 src/raspiProject.py --kill'], shell=True)
-    return True
+def stop_script(background_tasks: BackgroundTasks):
+    background_tasks.add_task(kill_script)
+    return {"status": "Script stopped"}
 
 ### TEST PROGRAMM
 @app.post('/test')
-async def test():
-    sp = subprocess.call(['python3 src/raspiProject.py --test'], shell=True)
-    return True
+def start_test_script(background_tasks: BackgroundTasks):
+    background_tasks.add_task(test_script)
+    return {"status": "Start test"}
+
 
 ### CHECK PROGRAMM STATE
 @app.get('/check')
-async def check():
+def check():
     if process['id']:
         return True
     return False
 
 ### LIST REPORTS
 @app.get('/reports/list')
-async def list_reports():
+def list_reports():
     reports = os.listdir('src/tests/reports/')
     return reports
 
 ### GET REPORT
 @app.get('/reports/{filename}')
-async def download_report(filename: str):
+def download_report(filename: str):
     return FileResponse(path=f'src/tests/reports/{filename}', media_type='application/octet-stream')
 
 ### TEMPERATURE
 @app.get('/temperature')
-async def get_temperature():
+def get_temperature():
     temperature = round(bmp280.get_temperature(), 1)
     return temperature
 
 ### STATE
 @app.get('/state/touch')
-async def get_touch_state():
+def get_touch_state():
     return touch.state()
 
 @app.get('/state/tilt')
-async def get_tilt_state():
+def get_tilt_state():
     return tilt.state()
 
 @app.get('/state/fan')
-async def get_fan_state():
+def get_fan_state():
     return fan.state()
