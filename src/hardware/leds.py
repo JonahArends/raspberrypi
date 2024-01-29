d7b34231e92e0325ebb9572423d2859c3e16a534
Jonah <122887053+JonahArends@users.noreply.github.com>
2024-01-29 19:01:05 +0000

    fix: leds

--- src/hardware/leds.py
+++ src/hardware/leds.py
@@ -34,7 +34,7 @@ class LED:
     def active(self):
         inactive_leds = {}
         for led_pin in self.led_pins:
-            current_state = GPIO.input(led_pin)
+            current_state = GPIO.output(led_pin)
             if current_state == GPIO.LOW:
                 inactive_leds[led_pin] = True
             inactive_leds[led_pin] = False
